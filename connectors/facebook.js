8099292ff1727813dc634e4eaa621a03ac14febb
Hai Le Phu <lephuhai0511@gmail.com>
2017-03-23 09:17:02 +0700

    Fix facebook function handle message type

--- connectors/facebook.js
+++ connectors/facebook.js
@@ -51,21 +51,19 @@ const sendMessageType = function (recipientId, type, payload) {
     message: {
       attachment: {
         type: 'template',
-        payload: payload
+        payload: {}
       }
     }
   }
-  if (payload.quick_replies && payload.quick_replies.length > 0) {
-    messageData.message.attachment.payload.template_type = 'button'
-    messageData.message.attachment.payload.text = payload.text
-    messageData.message.attachment.payload.buttons = payload.quick_replies.map(function (item) {
-      return {
-        type: 'postback',
-        title: item,
-        payload: slug(item, '_')
-      }
-    })
-  }
+  messageData.message.attachment.payload.template_type = 'button'
+  messageData.message.attachment.payload.text = payload.text
+  messageData.message.attachment.payload.buttons = payload.quick_replies.map(function (item) {
+    return {
+      type: 'postback',
+      title: item,
+      payload: slug(item, '_')
+    }
+  })
 
   callSendAPI(messageData)
 }
